a2f7f09a5d2003e77c8815d7263e174dbc6a423a
zHaytam <haytam.zanid@gmail.com>
2020-05-16 22:26:29 +0000

    Add PropertyGetter unit tests

---
+++ DynamicExpressions.UnitTests/PropertyGetterTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace DynamicExpressions.UnitTests
+{
+    public class PropertyGetterTests
+    {
+        [Fact]
+        public void GetPropertyGetter_ShouldThrow_WhenPropertyIsNull()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                DynamicExpressions.GetPropertyGetter<PropertyGetterTests>(null);
+            });
+
+            Assert.Equal("Value cannot be null. (Parameter 'property')", ex.Message);
+        }
+
+        [Fact]
+        public void GetPropertyGetter_ShouldReturnCorrectGetter()
+        {
+            var entry = new Entry { Id = 2 };
+
+            var getter = DynamicExpressions.GetPropertyGetter<Entry>("Id").Compile();
+            var id = getter(entry);
+
+            Assert.Equal(entry.Id, id);
+        }
+
+        [Fact]
+        public void GetPropertyGetter_ShouldBeUsableInQueryableOrderBy()
+        {
+            var entries = new List<Entry>
+            {
+                new Entry { Id = 1 },
+                new Entry { Id = 2 },
+            };
+
+            var getter = DynamicExpressions.GetPropertyGetter<Entry>("Id");
+            var sortedEntries = entries.AsQueryable().OrderByDescending(getter).ToList();
+
+            Assert.Equal(entries[0], sortedEntries[1]);
+            Assert.Equal(entries[1], sortedEntries[0]);
+        }
+
+        [Fact]
+        public void GetPropertyGetter_ShouldThrow_WhenPropertyDoesntExist()
+        {
+            var entry = new Entry { Id = 2 };
+
+            var ex = Assert.Throws<ArgumentException>(() => DynamicExpressions.GetPropertyGetter<Entry>("Test"));
+
+            Assert.Equal("Instance property 'Test' is not defined for type 'DynamicExpressions.UnitTests.Entry' (Parameter 'propertyName')", ex.Message);
+        }
+    }
+
+    internal class Entry
+    {
+        public int Id { get; set; }
+    }
+}
